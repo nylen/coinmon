17b08a5b3ee7c1cbaa6bb908ca562912e47a366f
James Nylen <jnylen@gmail.com>
2011-07-12 19:22:26 -0400

    Set both fans on full

--- other/set-oc.sh
+++ other/set-oc.sh
@@ -2,4 +2,4 @@
 
 aticonfig --od-setclocks=950,1050 --adapter=1
 DISPLAY=:0.0 aticonfig --pplib-cmd 'set fanspeed 0 100'
-#DISPLAY=:0.1 aticonfig --pplib-cmd 'set fanspeed 0 100'
+DISPLAY=:0.1 aticonfig --pplib-cmd 'set fanspeed 0 100'
