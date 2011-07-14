a06ee1f544517099415f4b50bc933b1d2b44ac8f
James Nylen <jnylen@gmail.com>
2011-07-13 23:08:39 -0400

    Make set-oc.sh script set $DISPLAY

--- other/set-oc.sh
+++ other/set-oc.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-aticonfig --od-setclocks=950,1050 --adapter=1
+DISPLAY=:0 aticonfig --od-setclocks=950,1050 --adapter=1
+
 DISPLAY=:0.0 aticonfig --pplib-cmd 'set fanspeed 0 100'
 DISPLAY=:0.1 aticonfig --pplib-cmd 'set fanspeed 0 100'
